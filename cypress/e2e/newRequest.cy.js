1ef7103bb56843a9c8b71e4c06563fa1d69964f2
chyedriansa <chyedriansa@gmail.com>
2024-05-12 03:00:44 +0700

    feat:Adding random pick data

--- cypress/e2e/newRequest.cy.js
+++ cypress/e2e/newRequest.cy.js
@@ -1,19 +1,19 @@
 //fungsi membuat random data
-var randomData = ["CY1" , "CY2" , "CY3" , "CY4" , "CY5" , "0000001" , "0000001"]
-var x = Math.floor((Math.random() *6)) +1;
+var randomData = ["CY1", "CY2", "CY3", "CY4", "CY5", "0000001", "0000001"]
+var x = Math.floor((Math.random() * 6)) + 1;
 
 
 describe('Login Case', () => {
 
     //inisiasi random data
-    var _InvoiceNumber = randomData [x] 
+    var _InvoiceNumber = randomData[x]
     beforeEach(() => {
         cy.visit('https://samaktamitrapt-dev.outsystemsenterprise.com/ClaimPortal/')
         cy.login('nancy.martin', '123456')
     })
 
-    it('Positive : New Request' , () => {
-
+    //kondisi jika data jika valid 
+    it('Positive : New Request', () => {
         cy.contains('Invoices').click()
         cy.contains('button', 'New Request').click()
         cy.get('#Input_InvoiceNumber').type(_InvoiceNumber)
@@ -22,8 +22,9 @@ describe('Login Case', () => {
         cy.get('#Input_Amount').type('250000')
         cy.contains('button', 'Submit').click()
     })
-    //
-    it('Negative : New Request' , () => {
+
+    //kondisi jika data tidak valid
+    it('Negative : New Request', () => {
         cy.contains('Invoices').click()
         cy.contains('button', 'New Request').click()
         cy.get('#Input_InvoiceNumber').type(_InvoiceNumber)
@@ -33,5 +34,5 @@ describe('Login Case', () => {
         cy.get('#Input_Amount').type('250000')
         cy.contains('button', 'Submit').click()
     })
-    
+
 })
